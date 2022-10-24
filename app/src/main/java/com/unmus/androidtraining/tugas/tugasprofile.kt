db5c2947a43df9cee69486210aed7f6cf823cd8c
Diyah Dewi lestari <ddlestari2008@gmail.com>
2022-10-24 16:00:39 +0900

    search bar

--- app/src/main/java/com/unmus/androidtraining/tugas/tugasprofile.kt
+++ app/src/main/java/com/unmus/androidtraining/tugas/tugasprofile.kt
@@ -24,6 +24,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.unmus.androidtraining.ui.theme.AndroidTrainingTheme
 import com.unmus.androidtraining.ui.theme.Purple500
+import com.unmus.androidtraining.R
 
 class tugasprofile : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
